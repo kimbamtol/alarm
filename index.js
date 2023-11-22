6e2edfcfc942cc55da4be65509e6a81b0b30badd
kimbamtol0304 <kimbamtol0304@gmail.com>
2023-11-22 22:14:37 +0900

    1차 테스트 완료 / notification 성공

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { registerRootComponent } from 'expo';
-
 import App from './App';
+import PushNotification from 'react-native-push-notification';
+
+// 앱이 시작될 때 실행되는 함수
+const onAppStart = () => {
+    PushNotification.localNotification({
+        message: '앱이 시작되었습니다!',
+    });
+};
+
+// 앱이 시작될 때 실행되는 함수 호출
+onAppStart();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
